db67d4833577162c84d138d5cd3240544155a99e
Seunfola <seunfola1@gmail.com>
2024-09-28 20:25:26 +0100

    fixing the mini component

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '@/styles/components/button.module.scss';
+import styles from '@/styles/components/Button.module.scss';
 
 interface ButtonProps {
   text: string;
@@ -7,15 +7,31 @@ interface ButtonProps {
   className?: string;
   type?: 'button' | 'submit' | 'reset';
   disabled?: boolean;
+  bgColor?: string;  
+  color?: string;    
+  variant?: 'solid' | 'outlined'; 
 }
 
-const Button: React.FC<ButtonProps> = ({ text, onClick, className, type = 'button', disabled = false }) => {
+const Button: React.FC<ButtonProps> = ({
+  text,
+  onClick,
+  className,
+  type = 'button',
+  disabled = false,
+  bgColor = '#2f4454',
+  color = 'white',
+  variant = 'solid',  
+}) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`${styles.button} ${className}`}
+      className={`${styles.button} ${styles[variant]} ${className}`}
       disabled={disabled}
+      style={{
+        '--button-bg-color': bgColor,
+        '--button-color': color,
+      } as React.CSSProperties}
     >
       {text}
     </button>
--- src/app/guest/components/welcomePage.tsx
+++ src/app/guest/components/welcomePage.tsx
@@ -1,11 +1,31 @@
+import Button from "@/app/components/Button";
+import styles from "@/styles/dashboard/guest/components/welcomepage.module.scss"; 
+
 const WelcomePage = () => {
-    return ( 
-        <div>
-             <h1 >Conneting Ambition with Oppourtunity</h1>
-        <p>Welcome to Fodelahire where we transform</p>
-        </div>
-     );
-}
- 
-export default WelcomePage
-;
+  return ( 
+    <div className={styles.welcomePage}>
+      <h1 className={styles.title}>Connecting Ambition with Opportunity</h1>
+      <p className={styles.description}>
+        Welcome to Fodelahire, where we transform careers and empower businesses. As industry leaders in recruitment and talent acquisition, we bridge the gap between exceptional candidates and forward-thinking companies.
+      </p>
+      <div className={styles.btnContainer}>
+        <Button 
+          type="submit" 
+          text="Find Jobs" 
+          variant="solid"     
+          bgColor="#2f4454"   
+          color="white"       
+        />
+        <Button 
+          type="submit" 
+          text="Hire a Freelancer" 
+          variant="outlined"  
+          bgColor="#2f4454"   
+          color="#2f4454"     
+        />
+      </div>
+    </div>
+  );
+};
+
+export default WelcomePage;
