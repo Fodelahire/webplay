75d2a0ba590ea4de43be0cb5b8e0e94332b19672
Seunfola <seunfola1@gmail.com>
2024-09-29 23:26:45 +0100

    added the footer component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styles from './footer.module.scss';
+
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.logo}>
+        <img src="/logo.svg" alt="Fodelahire Logo" />
+        <p>Connecting Ambition with Opportunity</p>
+      </div>
+
+      <div className={styles.footerSection}>
+        <div>
+          <p className={styles.sectionTitle}>Products</p>
+          <ul>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Freelancers</a></li>
+            <li><a href="#">Certifications</a></li>
+          </ul>
+        </div>
+        <div>
+          <p className={styles.sectionTitle}>Company</p>
+          <ul>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Career</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Donate</a></li>
+            <li><a href="#">Enquiries</a></li>
+            <li><a href="#">Testimonials</a></li>
+          </ul>
+        </div>
+        <div>
+          <p className={styles.sectionTitle}>Resources</p>
+          <ul>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Help center</a></li>
+            <li><a href="#">Tutorials</a></li>
+            <li><a href="#">Support</a></li>
+          </ul>
+        </div>
+        <div>
+          <p className={styles.sectionTitle}>Legal</p>
+          <ul>
+            <li><a href="#">Terms</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">Cookies</a></li>
+            <li><a href="#">Licenses</a></li>
+            <li><a href="#">Settings</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className={styles.subscribeSection}>
+        <p className={styles.subscribeText}>Join our Slack community to connect with others and stay updated with the latest opportunities.</p>
+        <div className={styles.subscribeInput}>
+          <input type="email" placeholder="Enter your email" />
+          <button></button>
+        </div>
+        <div className={styles.socialIcons}>
+          
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
