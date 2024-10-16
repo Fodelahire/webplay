6eff4b6267a19ae99fa58fdf482015fd5a976d3f
Seunfola <seunfola1@gmail.com>
2024-10-16 12:11:52 +0100

    update the api by commenting the exiisting code out

--- src/api/review/route.ts
+++ src/api/review/route.ts
@@ -1,19 +1,19 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '../../../lib/prisma'; 
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import prisma from '../../../lib/prisma'; 
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    try {
-      const reviews = await prisma.review.findMany({
-        take: 6, 
-      });
-      res.status(200).json(reviews);
-    } catch (error) {
-      console.error('Failed to fetch reviews:', error);
-      res.status(500).json({ message: 'Failed to fetch reviews' });
-    }
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method === 'GET') {
+//     try {
+//       const reviews = await prisma.review.findMany({
+//         take: 6, 
+//       });
+//       res.status(200).json(reviews);
+//     } catch (error) {
+//       console.error('Failed to fetch reviews:', error);
+//       res.status(500).json({ message: 'Failed to fetch reviews' });
+//     }
+//   } else {
+//     res.setHeader('Allow', ['GET']);
+//     res.status(405).end(`Method ${req.method} Not Allowed`);
+//   }
+// }
